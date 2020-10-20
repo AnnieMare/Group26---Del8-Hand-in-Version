cfb30e9a3c4eb66635009f1cd6c7b9be41ef67a8
IzaanvZyl <50364349+IzaanvZyl@users.noreply.github.com>
2020-10-20 04:32:52 +0200

    Update EmailString.cs

--- RC API/RC API/Models/EmailString.cs
+++ RC API/RC API/Models/EmailString.cs
@@ -7,7 +7,6 @@ namespace RC_API.Models
 {
     public class EmailString
     {
-        public string EmailKey = "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";
-        public string FromEmail = "u17054002@tuks.co.za";
+        
     }
 }
